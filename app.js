5cfead9c31a13f6923f27a9c69fb4ac300fd32e6
Kashish Arora <arorakashish311@gmail.com>
2022-12-04 12:56:55 +0530

    Updated app js file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let searchTxt= document.getElementById('searchTxt');
 
 //Event listeners
 addbtn.addEventListener('click', addaNote);
-// searchTxt.addEventListener('input', searchtext); This field was disabled in html previously and was causing Error
+searchTxt.addEventListener('input', searchtext)
 
 //Functions
 //let notesArray=[];
